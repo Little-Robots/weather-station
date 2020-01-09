f14da6b268c62646b5eeeb9c0fbf4ad121b8da2b
Tomás Migone <tomasmigone@gmail.com>
2020-01-09 20:02:17 -0300

    Funcionando todo salvo RFID y buzzer
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- weather-station.ino
+++ weather-station.ino
@@ -23,7 +23,22 @@
 #define PIN_WATER_SENSOR A0
 #define PIN_LIGHT_SENSOR A1
 
+// IR Remote command keys
+#define IR_COMMAND_1 0x9716BE3F
+#define IR_COMMAND_2 0x3D9AE3F7
+#define IR_COMMAND_3 0x6182021B
+#define IR_COMMAND_4 0x8C22657B
+#define IR_COMMAND_5 0x488F3CBB
+
+// Liquid Crystal Display
 LiquidCrystal lcd(PIN_LCD_RS, PIN_LCD_EN, PIN_LCD_D4, PIN_LCD_D5, PIN_LCD_D6, PIN_LCD_D7);
+
+// Infrared Remote
+IRrecv irrecv(PIN_IR_REMOTE);
+decode_results ir_command;
+
+// DHT11 sensor
+SimpleDHT11 dht11(PIN_DHT11);
 
 // Measured variables
 byte temperature = 0;
@@ -31,19 +46,28 @@ byte humidity = 0;
 byte water = 0;
 byte light = 0;
 
+long last = 0;
+long mode_timer = 0;
+long time_temp = 0;
+
 // Modes
 enum modes
 {
   MODE_TEMPERATURE,
   MODE_HUMIDITY,
   MODE_LIGHT,
-  MODE_WATER
+  MODE_WATER,
+  MODE_RANDOM
 };
+
+int selected = MODE_TEMPERATURE;
 int display_mode = MODE_TEMPERATURE;
 
 void setup()
 {
-  setup_LCD();
+  // Serial.begin(9600);
+  lcd.begin(16, 2);
+  irrecv.enableIRIn();
 
   // Print a boot message
   print_LCD("*** WEATHER ***", "*** STATION ***");
@@ -53,8 +77,65 @@ void setup()
 
 void loop()
 {
+  read_irremote();
   read_sensors();
   update_LCD();
+}
+
+void read_irremote()
+{
+  if (irrecv.decode(&ir_command))
+  {
+    Serial.println(ir_command.value, HEX);
+    switch (ir_command.value)
+    {
+    case IR_COMMAND_1:
+      display_mode = MODE_TEMPERATURE;
+      break;
+
+    case IR_COMMAND_2:
+      display_mode = MODE_WATER;
+      break;
+
+    case IR_COMMAND_3:
+      display_mode = MODE_LIGHT;
+      break;
+
+    case IR_COMMAND_4:
+      display_mode = MODE_HUMIDITY;
+      break;
+
+    case IR_COMMAND_5:
+      display_mode = MODE_RANDOM;
+      break;
+
+    default:
+      break;
+    }
+    irrecv.resume();
+  }
+}
+
+void readTemperatureAndHumiditySensor()
+{
+
+  if ((millis() - time_temp) > (1000))
+  { // 1 SEC
+    time_temp = millis();
+    TIMSK2 = 0x00;
+    int err = dht11.read(&temperature, &humidity, NULL);
+    TIMSK2 = 0x02;
+  }
+}
+
+void readWaterSensor()
+{
+  water = map(analogRead(PIN_WATER_SENSOR), 0, 640, 0, 100); // 640 TOP LIMIT
+}
+
+void readLightSensor()
+{
+  light = map(min(analogRead(PIN_LIGHT_SENSOR), 512), 0, 512, 0, 100); // 512 es mucha luz ya
 }
 
 void read_sensors()
@@ -64,13 +145,13 @@ void read_sensors()
   readTemperatureAndHumiditySensor();
 }
 
-void setup_LCD()
+void print_LCD(String top_row, String bottom_row)
 {
-  lcd.begin(16, 2);
-}
-
-void print_LCD(char *top_row, char *bottom_row)
-{
+  if ((millis() - last) > (1000 / 20))
+  { //FPS TOP
+    last = millis();
+    lcd.clear();
+  }
   lcd.setCursor(0, 0);
   lcd.print(top_row);
   lcd.setCursor(0, 1);
@@ -79,21 +160,45 @@ void print_LCD(char *top_row, char *bottom_row)
 
 void update_LCD()
 {
-  switch(display_mode) {
-    case MODE_TEMPERATURE:
-      print_LCD("Temperature: ", String(temperature) + String(" \337C"));
-      break;
-    case MODE_HUMIDITY:
-      print_LCD("Humidity: ", String(humidity) + String(" %"));
-      break;
-    case MODE_LIGHT:
-      print_LCD("Light: ", String(light) + String(" lumens"));
-      break;
-    case MODE_WATER:
-      print_LCD("Water: ", String(water) + String(" %"));
-      break;
-    default:
-      print_LCD("", "");
-      break;
+
+  if ((millis() - mode_timer) > (1000))
+  { // 1 SEC
+    mode_timer = millis();
+    selected++;
+    if (selected > MODE_WATER)
+    {
+      selected = MODE_TEMPERATURE;
+    }
+  }
+
+  switch (display_mode)
+  {
+  case MODE_TEMPERATURE:
+    print_LCD("Temperature: ", String(temperature) + String(" \337C"));
+    break;
+  case MODE_HUMIDITY:
+    print_LCD("Humidity: ", String(humidity) + String(" %"));
+    break;
+  case MODE_LIGHT:
+    print_LCD("Light: ", String(light) + String(" lumens"));
+    break;
+  case MODE_WATER:
+    print_LCD("Water: ", String(water) + String(" %"));
+    break;
+  case MODE_RANDOM:
+    if ((millis() - mode_timer) > (1000))
+    { // 1 SEC
+      mode_timer = millis();
+      selected++;
+      if (selected > MODE_WATER)
+      {
+        selected = MODE_TEMPERATURE;
+      }
+    }
+    display_mode = selected;
+    break;
+  default:
+    print_LCD("", "");
+    break;
   }
 }
